994778fbe58cb7f910a0f836858055aee0e196c6
Abinet <agonaferdegefe@gmail.com>
2026-03-30 16:05:41 +0300

    Prepare go modules and set up endpoints for each logic

---
+++ competitionWithGo/go.mod
@@ -0,0 +1,3 @@
+module competitionService
+
+go 1.22.2
--- competitionWithGo/main.go
+++ competitionWithGo/main.go
@@ -1,9 +1,62 @@
 package main
 
-import "fmt"
 
+import (
+
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader(
+
+	CheckOrigin: func(r *http.Request)bool{ return true }
+)
+
+type Question struct {
+
+	QuestionId int,
+	Subject string,
+        Answer string
+	Text string
+
+}
+func sendQuestions(w http.ResponseWriter, r *http.Request){
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+
+		fmt.Println("Error creating connection ", err)
+		return
+	}
+
+}
+
+func login(){
+
+}
+
+func evaluateAnswer(w http.ResponseWriter, r *http.Request){
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil { 
+		fmt.Println("Error creating connection ", err)
+		return
+	}
+
+}
+
+func announceWinner(){
+
+}
 
 func main(){
+        
+	http.HandleFunc("/questions", sendQuestions)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/respond", evaluateAnswer)
+	http.HandleFunc("/result", announceWinner)
 
-	fmt.Println("working .....")
+	questionChan := make(chan Question)
+	fmt.Println("hellow go")
 }
