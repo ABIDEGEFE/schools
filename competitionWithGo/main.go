d120ebc9c94e550341cc5333cd86e4a2a0bb7f06
Abinet <agonaferdegefe@gmail.com>
2026-04-01 16:36:52 +0300

    configure database attachment through gorm

--- competitionWithGo/main.go
+++ competitionWithGo/main.go
@@ -4,24 +4,39 @@ package main
 import (
 
 	"fmt"
-	"net/http"
-	"github.com/gorilla/websocket"
+	// "net/http"
+	// "github.com/gorilla/websocket"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
 )
 
-var upgrader = websocket.Upgrader(
+type User struct {
 
-	CheckOrigin: func(r *http.Request)bool{ return true }
-)
+	ID uint `gorm:"primaryKey"`
+	Username string `gorm:"size:255;not null"`
+	Email string `gorm:"uniqueIndex;not null"`
+}
 
 type Question struct {
 
-	QuestionId int,
-	Subject string,
+	Qid uint `gorm:"primaryKey"`
+	Text string `gorm:"not null"`
+	Answer string `gorm:"not null"`
+}
+
+/*var upgrader = websocket.Upgrader(
+
+	// CheckOrigin: func(r *http.Request)bool{ return true }
+)*/
+
+/*type Question struct {
+
+	QuestionId int
         Answer string
 	Text string
 
-}
-func sendQuestions(w http.ResponseWriter, r *http.Request){
+}*/
+/*func sendQuestions(w http.ResponseWriter, r *http.Request){
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -48,15 +63,22 @@ func evaluateAnswer(w http.ResponseWriter, r *http.Request){
 
 func announceWinner(){
 
-}
+}*/
 
 func main(){
         
-	http.HandleFunc("/questions", sendQuestions)
+	/*http.HandleFunc("/questions", sendQuestions)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/respond", evaluateAnswer)
-	http.HandleFunc("/result", announceWinner)
+	http.HandleFunc("/result", announceWinner)*/
 
-	questionChan := make(chan Question)
+	dsn := "host=localhost user=postgres password=12345678 dbname=competition port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil { panic("Failed to connect") }
+	db.AutoMigrate(&User{})
+        db.AutoMigrate(&Question{})
+
+	//questionChan := make(chan Question)
 	fmt.Println("hellow go")
 }
